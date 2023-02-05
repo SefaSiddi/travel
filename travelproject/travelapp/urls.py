23b9bd1cf65afafacd48a8c08c3855f74d640ee4
SefaSiddi <sefanasiddi@gmail.com>
2023-02-05 20:31:34 +0530

    new

---
+++ travelproject/travelapp/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+
+    path('',views.demo,name='demo'),
+
+    # path('add/',views.addition,name='addition'),
+]
